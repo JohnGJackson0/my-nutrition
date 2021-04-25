cfa67ce035c4f49499603b755f7e0e5027c9ee79
jgjio <johnGJackson0@gmail.com>
2021-04-24 23:01:03 -0400

    Convert login to use hooks

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,56 +1,37 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { View } from "react-native";
 import { Button, Input } from "react-native-elements";
+
 
 interface PropsType {
   onSignInHandler: (email: string, password: string) => void;
 }
 
-interface StateType {
-  email: string;
-  password: string;
-}
+export const Login: React.FC<PropsType> = (props) => {
+  const [email, setEmail] = useState<string | undefined>(undefined);
+  const [password, setPassword] = useState<string|undefined>(undefined);
 
-export class Login extends React.Component<PropsType, StateType> {
-  state = {
-    email: "",
-    password: "",
+  const onSubmit = () => {
+    props.onSignInHandler(email, password);
   };
 
-  handleChangeEmail = (email: string) => {
-    this.setState({ email });
-  };
-
-  handleChangePassword = (password: string) => {
-    this.setState({ password });
-  };
-
-  handleSubmit = () => {
-    const { email, password } = this.state;
-    this.props.onSignInHandler(email, password);
-  };
-
-  render() {
-    return (
-      <View>
-        <Input
-          placeholder="Email"
-          value={this.state.email}
-          onChangeText={this.handleChangeEmail}
-          leftIcon={<Icon name="email-outline" size={24} color="grey" />}
-          onSubmitEditing={this.handleSubmit}
-        />
-        <Input
-          placeholder="Password"
-          secureTextEntry
-          value={this.state.password}
-          onChangeText={this.handleChangePassword}
-          leftIcon={<Icon name="lock-outline" size={24} color="grey" />}
-          onSubmitEditing={this.handleSubmit}
-        />
-        <Button onPress={this.handleSubmit} title="Sign In" type="solid" />
-      </View>
-    );
-  }
+  return (
+    <View>
+      <Input
+        placeholder="Email"
+        value={email}
+        onChangeText={(value)=> setEmail(value)}
+        leftIcon={<Icon name="email-outline" size={24} color="grey" />}
+      />
+      <Input
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={(value) => setPassword(value)}
+        leftIcon={<Icon name="lock-outline" size={24} color="grey" />}
+      />
+      <Button onPress={onSubmit} title="Sign In" type="solid" />
+    </View>
+  );
 }
